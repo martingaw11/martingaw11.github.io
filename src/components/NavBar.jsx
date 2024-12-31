c8778e5ef38c62488601728212966093358a347e
Martin Gawron <martingaw11@gmail.com>
2024-12-30 21:27:14 -0600

    tailwind import and navbar creation

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+  return (
+    <>
+      <div class="flex justify-around items-center bg-vanillaberry max-w-sm w-full py-3 px-2 rounded-lg" >
+        <a class="navbar-link" href="#" >Home</a>
+        <a class="navbar-link" href="#" >Projects</a>
+        <a class="navbar-link" href="#" >About</a>
+      </div>
+    </>
+  )
+}
+
+export default NavBar;
