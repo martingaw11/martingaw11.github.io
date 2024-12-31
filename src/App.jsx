3926c51c1a394b4a1ebbcd56ed2d35d3da4d994e
Martin Gawron <martingaw11@gmail.com>
2024-12-31 14:05:00 -0600

    dynamic sizing and init body

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ function App() {
   
 
   return (
-    <>
-      <NavBar/>
-    </>
+    <div class="flex-col">
+      <div id="navbar" class="navbar-grow fixed top-5 w-80 flex justify-end" >        
+        <NavBar styles="w-full" />
+      </div>
+      <div id="body" class="body-grow mx-auto mt-24">
+        <h1 class="relative inline text-3xl py-2 text-vanillaberry font-bold">
+            Martin Gawron
+          </h1>
+      </div>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
-const NavBar = () => {
+import React from 'react';
+
+const NavBar = ({styles}) => {
 
   return (
     <>
-      <div class="flex justify-around items-center bg-vanillaberry max-w-sm w-full py-3 px-2 rounded-lg" >
+      <div class={`flex justify-around bg-vanillaberry max-w-sm py-3 px-2 rounded-lg ${styles}`} >
         <a class="navbar-link" href="#" >Home</a>
         <a class="navbar-link" href="#" >Projects</a>
         <a class="navbar-link" href="#" >About</a>
