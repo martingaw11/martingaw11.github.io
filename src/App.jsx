2642e9dde6db1201d6a054a1fb1789e907c92313
Martin Gawron <martingaw11@gmail.com>
2025-01-02 21:48:01 -0600

    add about-me and init skillsCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import NavBar from './components/NavBar'
+import SkillsCard from './components/SkillsCard'
 import TypingTextList from './components/TypingTextList'
 import './index.css'
 
 const attr = [
-  "Developer Intern w/ Intelligent Automation @ CIC",
+  "Dev Intern w/ Intelligent Automation @ The Cincinnati Insurance Companies",
   "Computer Science and Software Engineering @ UIC",
-  "Full Stack Developer",
-  "Web Developer",
+]
+
+const skillSet = [
+  "React", "Python", "Java", "HTML", "CSS", "JavaScript"
 ]
 
 function App() {
@@ -18,10 +21,19 @@ function App() {
         <NavBar styles="w-full" />
       </div>
       <div id="body" className="body-grow max-w-full">
-        <h1 className="relative inline text-3xl py-2 text-vanillaberry font-bold">
-            Martin Gawron
-        </h1>
-        <TypingTextList texts={attr} styles="pt-2 pl-5" />
+        <div id="header" className="min-h-36" >
+          <h1 className="relative inline text-3xl py-2 text-vanillaberry font-bold">
+              Martin Gawron
+          </h1>
+          <TypingTextList texts={attr} typingSpeed={90} pauseTime={750} styles="indent-grow py-2" />
+        </div>
+        <div id="about-me" >
+          <h2 id="about-me-title" className="text-2xl text-vanillaberry font-bold" >About Me</h2>
+          <p id="about-me-text" className="indent-grow py-2 text-lg" >
+            I am a Computer Science and Software Engineering student at the University of Illinois at Chicago. I am currently a Developer Intern at The Cincinnati Insurance Companies (CIC) where I work with the Intelligent Automation team. I like to do Full Stack Development with a focus on making Web Applications. I am passionate about learning new technologies and building applications that solve real-world problems. I am always looking for new opportunities to grow and expand my skill set.
+          </p>
+          <SkillsCard skillSet={skillSet} styles="mt-10" />
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ export default {
     extend: {
       colors: {
         vanillaberry: "#53b2c2",
+        blueberry: "#282874",
+      },
+      boxShadow: {
+        'DEFAULT': '0 5px 5px rgba(0, 0, 0, 0.15)',
+        'xl': '0 5px 15px 2px rgba(40, 40, 116, 0.4)'
       }
     },
   },
